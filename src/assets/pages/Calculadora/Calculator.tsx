3edf75b3006b9ec914977e02b2c9ff47c01e69ab
Abraão Santos <115851871+TechAbraao@users.noreply.github.com>
2024-07-27 16:08:27 -0300

    Uma nova proposta de código

--- src/assets/pages/Calculadora/Calculator.tsx
+++ src/assets/pages/Calculadora/Calculator.tsx
@@ -10,20 +10,34 @@ const BottonsCalculator = () => {
    //
    const [selectedNumber, setSelectNumber] = useState<number | string>("");
    const [oldSelectNumber, oldSetSelectNumber] = useState<number | string>("");
-   const [op, setOp] = useState<React.ReactNode>("")
-   const [selectedOperation, setSelectOperation] = useState<string>("");
+   const [op, setOp] = useState<React.ReactNode>("");
+   const [isVerified, setIsVerified] = useState<boolean>(true);
+   // const [selectedOperation, setSelectOperation] = useState<string>("");
    // Funções
-   const handleNumber = (index: number) => {
-      if (selectedNumber === "0") {
-         index.toString();
+   const verificaIndex = (index: number) => {
+      console.log(`Index: ${index}`);
+   };
+   const handleNumber = (index: number, numberItem: number) => {
+      if (
+         index === 4 ||
+         index === 5 ||
+         index === 6 ||
+         index === 8 ||
+         index === 9 ||
+         index === 10 ||
+         index === 12 ||
+         index === 13 ||
+         index === 14 ||
+         index === 16
+      ) {
+         console.log(`Index do HandleNumber: ${index}`);
+         setSelectNumber(selectedNumber.toString() + numberItem.toString());
       } else {
-         setSelectNumber(" ");
-         const x: string = selectedNumber.toString() + index.toString();
-         setSelectNumber(x);
+         //
       }
    };
    const clearAll = (index: string | number) => {
-      index === 0 ? setSelectNumber("") : null;
+      index === 0 && setSelectNumber(""), oldSetSelectNumber("")
    };
    //
    const changeSignal = (index: number) => {
@@ -58,124 +72,69 @@ const BottonsCalculator = () => {
          console.log("Vazio");
       }
    };
-   //
-   const operatorResult = (index: number) => {
-      if (index === 18) {
-         switch (selectedOperation) {
-            case "adic":
-               setSelectNumber(
-                  (
-                     parseFloat(oldSelectNumber.toString()) +
-                     parseFloat(selectedNumber.toString())
-                  ).toString()
-               );
-               setSelectOperation("");
-               break;
-            case "subt":
-               setSelectNumber(
-                  (
-                     parseFloat(oldSelectNumber.toString()) -
-                     parseFloat(selectedNumber.toString())
-                  ).toString()
-               );
-               setSelectOperation("");
-               break;
-            case "mult":
-               setSelectNumber(
-                  (
-                     parseFloat(oldSelectNumber.toString()) *
-                     parseFloat(selectedNumber.toString())
-                  ).toString()
-               );
-               setSelectOperation("");
-               break;
-            case "divi":
-               setSelectNumber(
-                  (
-                     parseFloat(oldSelectNumber.toString()) /
-                     parseFloat(selectedNumber.toString())
-                  ).toString()
-               );
-               setSelectOperation("");
-               break;
-            default:
-               console.log("Operação não realizada. Verifique o erro.");
-         }
-      } else {
-         console.log("Errado");
-      }
-   };
-   //
-   const operatorCalculator = (index: number) => {
+   // Fazer a calculadora funcionar aqui
+   const opSelect = (index: number) => {
       if (index === 3 || index === 7 || index === 11 || index === 15) {
+         // oldSetSelectNumber(selectedNumber);
+         // setSelectNumber("")
          switch (index) {
             case 3:
-               console.log("Case 3: divisão");
-               setSelectOperation("divi"); // divi de divisão
+               setOp("÷");
                oldSetSelectNumber(selectedNumber);
                setSelectNumber("");
                break;
             case 7:
-               console.log("Case 7: multiplicação");
-               setSelectOperation("mult"); // mult de multiplicação
+               setOp("x");
                oldSetSelectNumber(selectedNumber);
                setSelectNumber("");
                break;
             case 11:
-               console.log("Caso 11: subtração");
-               setSelectOperation("subt"); // subt de subtração
+               setOp("-");
                oldSetSelectNumber(selectedNumber);
                setSelectNumber("");
                break;
             case 15:
-               console.log("Case 15: adição");
-               setSelectOperation("adic"); // adic de adição
+               setOp("+");
+
                oldSetSelectNumber(selectedNumber);
                setSelectNumber("");
                break;
-            default:
-               console.log("Case indeterminado.");
          }
       }
    };
    //
    const dataCalculator = [
-      { index: 1, operationSymbol: "AC" },
-      { index: 2, operationSymbol: "[+/-]" },
-      { index: 3, operationSymbol: <Percent size={45} color="white" /> },
+      { operationSymbol: "AC" },
+      { operationSymbol: "[+/-]" },
+      { operationSymbol: <Percent size={45} color="white" /> },
       {
-         index: 4,
          operationSymbol: <Divide size={45} color="white" />,
          operationSelect: "/",
       },
-      { index: 5, operationSymbol: "7", number: 7 },
-      { index: 6, operationSymbol: "8", number: 8 },
-      { index: 7, operationSymbol: "9", number: 9 },
+      { operationSymbol: "7", number: 7 },
+      { operationSymbol: "8", number: 8 },
+      { operationSymbol: "9", number: 9 },
       {
-         index: 8,
          operationSymbol: <Asterisk size={45} color="white" />,
          operationSelect: "*",
       },
-      { index: 9, operationSymbol: "4", number: 4 },
-      { index: 10, operationSymbol: "5", number: 5 },
-      { index: 11, operationSymbol: "6", number: 6 },
+      { operationSymbol: "4", number: 4 },
+      { operationSymbol: "5", number: 5 },
+      { operationSymbol: "6", number: 6 },
       {
-         index: 12,
          operationSymbol: <Minus size={45} color="white" />,
          operationSelect: "-",
       },
-      { index: 13, operationSymbol: "1", number: 1 },
-      { index: 14, operationSymbol: "2", number: 2 },
-      { index: 15, operationSymbol: "3", number: 3 },
+      { operationSymbol: "1", number: 1 },
+      { operationSymbol: "2", number: 2 },
+      { operationSymbol: "3", number: 3 },
       {
-         index: 16,
          operationSymbol: <Plus size={45} color="white" />,
          operationSelect: "+",
       },
-      { index: 17, operationSymbol: "0", number: 0 },
-      { index: 18, operationSymbol: "." },
+      { operationSymbol: "0", number: 0 },
+      { operationSymbol: "." },
       {
-         index: 19,
          operationSymbol: <Equal size={45} color="white" />,
          operationSelect: "=",
       },
@@ -183,19 +142,24 @@ const BottonsCalculator = () => {
    //
    return (
       <div className="bottonsCalculator">
+         <div className="operationsView">
+            <p>{oldSelectNumber}</p>
+            <p>{op}</p>
+            <p>{selectedNumber}</p>
+         </div>
          <Result selectedNumber={selectedNumber} />
          <ul>
             {dataCalculator.map((item, index) => (
                <li
                   key={index}
                   onClick={() => {
-                     handleNumber(item.number ?? 0);
+                     handleNumber(index, item.number);
                      clearAll(index);
                      changeSignal(index);
                      changePorc(index);
                      handleComma(index);
-                     operatorCalculator(index);
-                     operatorResult(index);
+                     opSelect(index);
+                     // verificaIndex(index);
                   }}
                >
                   <ButtonIcon symbol={item.operationSymbol} />
@@ -207,9 +171,9 @@ const BottonsCalculator = () => {
 };
 //
 type ResultProps = {
-   selectedNumber: string | number;
-   oldSelectNumber: string | number;
-   selectedOperation: string;
+   selectedNumber?: string | number;
+   oldSelectNumber?: string | number;
+   selectedOperation?: string;
 };
 const Result = ({ selectedNumber }: ResultProps) => {
    //
